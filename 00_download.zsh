6741430073501e9273a305bd6ef2bd4d8a21fd0b
matsurihi.me <webmaster@matsurihi.me>
2022-12-22 11:07:11 +0900

    Improve build workflows

---
+++ 00_download.zsh
@@ -0,0 +1,5 @@
+#!/usr/bin/env zsh
+
+curl -OL https://seed.line.me/src/images/fonts/LINE_Seed_JP.zip
+unzip LINE_Seed_JP.zip
+ln -s LINE_Seed_JP/Desktop/OTF otf
--- 02_cmap.zsh
+++ 02_cmap.zsh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-mkdir -p
+mkdir -p cmap
 for f in otf/*.otf; do
   fonttools ttx -t cmap -d cmap ${f}
 done
